f850f53e5f38f28d7b62e8f0f10f7c93342e3536
Guk Dmitry <gukdmitry.s@gmail.com>
2017-01-19 23:15:18 +0200

    ConnectTest

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Устанавливаем для сокета локальную конечную точку
+            //IPHostEntry ipHost = Dns.GetHostEntry("homeconnect");
+          
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("192.168.1.14"), 8080);
+
+            // Создаем сокет Tcp/Ip
+            Socket sListener = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Начинаем слушать соединенияl
+                while (true)
+                {
+                    Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint);
+
+                    // Программа приостанавливается, ожидая входящее соединение
+                    Socket handler = sListener.Accept();
+                    string data = null;
+
+                    // Мы дождались клиента, пытающегося с нами соединиться
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                    // Показываем данные на консоли
+                    Console.Write("Полученный текст: " + data + "\n\n");
+
+                    // Отправляем ответ клиенту\
+                    string Html = "<html><body><h1>It works!</h1></body></html>";
+                    // Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
+                    string Str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
+                    byte[] msg = Encoding.UTF8.GetBytes(Str);
+                    handler.Send(msg);
+
+                    if (data.IndexOf("<TheEnd>") > -1)
+                    {
+                        Console.WriteLine("Сервер завершил соединение с клиентом.");
+                        break;
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
